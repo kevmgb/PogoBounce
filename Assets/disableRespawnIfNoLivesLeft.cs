dcdd153b2251ee569461c3a312f964db7fb8e3da
Kevin K <46866870+kevmgb@users.noreply.github.com>
2020-10-09 22:50:51 +0300

    level 1-15

--- Assets/disableRespawnIfNoLivesLeft.cs
+++ Assets/disableRespawnIfNoLivesLeft.cs
@@ -18,19 +18,19 @@ public class disableRespawnIfNoLivesLeft : MonoBehaviour
         GameObject player = GameObject.Find("pogo");
         int pHealth = player.GetComponent<playerCollision>().health;
 
-        Debug.LogWarning("disable " + pHealth);
+        //Debug.LogWarning("disable " + pHealth);
 
         if (pHealth == 0)
         {
             myButton.interactable = false;
-            Debug.LogWarning("pHealth is zero");
+            //Debug.LogWarning("pHealth is zero");
         }
         else
         {
             myButton.interactable = true;
         }
 
-        Debug.Log("checkpoint has been clicked");
+        //Debug.Log("checkpoint has been clicked");
     }
 
     // Update is called once per frame
